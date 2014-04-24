752851ad15b5171e46c26ad69feaeec6f63f45e7
Christopher Kyle Horton <christhehorton@gmail.com>
2014-04-24 16:31:30 -0400

    Add userinput module.

---
+++ userinput.h
@@ -0,0 +1,27 @@
+#ifndef USERINPUT_H
+#define USERINPUT_H
+
+// Module which handles gathering interactive input from the user.
+
+#include <iostream>
+#include <string>
+
+namespace userinput
+{
+	// Gets the number of columns from the user.
+	int getColumns();
+	
+	// Gets the number of rows from the user.
+	int getRows();
+	
+	// Gets the title of the graph from the user.
+	string getTitle();
+	
+	// Gets the label for the vertical axis from the user.
+	string getVLabel();
+	
+	// Gets the label for the horizontal axis from the user.
+	string getHLabel();
+}
+
+#endif
