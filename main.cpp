d00730cf0157ce284cd62a459229fe9fa293d64c
Christopher Kyle Horton <christhehorton@gmail.com>
2014-04-24 15:44:39 -0400

    Added main.cpp file.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/*
+	ASCIIGraph - A commandline graphing utility.
+	Written by Christopher K. Horton and Nicholas Virag.
+*/
+
+int main(int argc, const char* argv[])
+{
+	// Stuff will go here
+	return 0;
+}
