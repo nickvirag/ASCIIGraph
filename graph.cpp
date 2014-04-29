beae78a30322e96bf7138d96fc78515783247d1b
Christopher Kyle Horton <christhehorton@gmail.com>
2014-04-29 16:48:29 -0400

    Add some member variables and functions.

--- graph.cpp
+++ graph.cpp
@@ -3,3 +3,32 @@
 Graph::Graph()
 {
 }
+
+void Graph::setDimensions(int rows, int cols)
+{
+	data.resize(rows);
+	for(int i = 0; i < rows; i++)
+	{
+		data.at(i).resize(cols);
+	}
+}
+
+void Graph::setTitle(string title)
+{
+	this->title = title;
+}
+
+void Graph::setVLabel(string vlabel)
+{
+	this->vlabel = vlabel;
+}
+
+void Graph::setHLabel(string hlabel)
+{
+	this->hlabel = hlabel;
+}
+
+void Graph::setPlottingChar(char plotting_char)
+{
+	this->plotting_char = plotting_char;
+}
--- graph.h
+++ graph.h
@@ -1,12 +1,26 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Graph
 {
 public:
 	Graph();
+	void setDimensions(int rows, int cols);
+	void setTitle(string title);
+	void setVLabel(string vlabel);
+	void setHLabel(string hlabel);
+	void setPlottingChar(char plotting_char);
 private:
-	
-}
+	vector< vector<float> > data;
+	string title;
+	string vlabel;
+	string hlabel;
+	char plotting_char;
+};
 
 #endif
