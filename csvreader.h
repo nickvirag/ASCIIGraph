95046fd00387263b24975dbd31f7d7039408e1fc
Christopher Kyle Horton <christhehorton@gmail.com>
2014-05-05 23:45:43 -0400

    Modify CSV Reader.

--- csvreader.h
+++ csvreader.h
@@ -17,7 +17,6 @@ public:
 	CSVReader();
 	void load(Graph* graph, string filename);	// Loads data from provided CSV file into graph
 private:
-	int determineColumns(string record);	// Determine the number of columns from provided line
 	void addRecordToGraph(Graph* graph, string record);	// Adds this record to the graph
 };
 
