0f0f26b4083939510d73891c9e39be2aff9c78e0
Christopher Kyle Horton <christhehorton@gmail.com>
2014-05-05 22:58:19 -0400

    Add commandline parsing and first part of main.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,54 @@
 	Written by Christopher K. Horton and Nicholas Virag.
 */
 
+#include <iostream>
+#include <string>
 #include "csvreader.h"
 #include "graph.h"
 #include "userinput.h"
 
+using namespace std;
+
 int main(int argc, const char* argv[])
 {
-	// Stuff will go here
+	string filename, title, vlabel, hlabel;
+	
+	switch(argc)
+	{
+	case 1:
+		// Ask the user for where input is coming from
+		cout << "Please enter the CSV filename: ";
+		getline(cin, filename);
+		break;
+	case 2:
+		// The filename is supplied as the first argument
+		filename = argv[1];
+		break;
+	default:
+		// Incorrect usage
+		cout << "Usage: ASCIIGraph [filename]" << endl;
+		return 1;
+	}
+	
+	// Get graph data
+	Graph* graph = new Graph();
+	CSVReader reader = new CSVReader();
+	reader.load(graph, filename);
+	delete reader;
+	
+	// Get graph details
+	cout << "Please enter the graph title: ";
+	getline(cin, title);
+	cout << "Please enter a vertical axis label: ";
+	getline(cin, vlabel);
+	cout << "Please enter a horizontal axis label: ";
+	getline(cin, hlabel);
+	
+	graph->setTitle(title);
+	graph->setVLabel(vlabel);
+	graph->setHLabel(hlabel);
+	
+	// TODO: Print completed graph
+	
 	return 0;
 }
